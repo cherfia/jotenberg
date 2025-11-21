d203d733a6caca0cf840e41dacc6891942cf3041
Taha Cherfia <taha.cherfia@gmail.com>
2025-11-21 23:49:12 +0100

    feat(common): add common parameters

--- src/main/java/dev/inaka/common/AbstractOptions.java
+++ src/main/java/dev/inaka/common/AbstractOptions.java
@@ -2,11 +2,11 @@ package dev.inaka.common;
 
 import dev.inaka.common.exceptions.FooterFileNotFoundExceptions;
 import dev.inaka.common.exceptions.HeaderFileNotFoundExceptions;
-import netscape.javascript.JSObject;
 import org.json.JSONObject;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -21,7 +21,17 @@ public abstract class AbstractOptions {
     protected final String extraHttpHeaders;
     protected final String failOnConsoleExceptions;
     protected final String failOnHttpStatusCodes;
+    protected final String failOnResourceHttpStatusCodes;
+    protected final String failOnResourceLoadingFailed;
     protected final String skipNetworkIdleEvent;
+    protected final String generateDocumentOutline;
+    protected final String cookies;
+    protected final String downloadFrom;
+    protected final String metadata;
+    protected final String split;
+    protected final String userPassword;
+    protected final String ownerPassword;
+    protected final List<File> embeds;
 
     protected AbstractOptions(Builder builder) {
         header = builder.header;
@@ -32,7 +42,17 @@ public abstract class AbstractOptions {
         extraHttpHeaders = builder.extraHttpHeaders;
         failOnConsoleExceptions = builder.failOnConsoleExceptions;
         failOnHttpStatusCodes = builder.failOnHttpStatusCodes;
+        failOnResourceHttpStatusCodes = builder.failOnResourceHttpStatusCodes;
+        failOnResourceLoadingFailed = builder.failOnResourceLoadingFailed;
         skipNetworkIdleEvent = builder.skipNetworkIdleEvent;
+        generateDocumentOutline = builder.generateDocumentOutline;
+        cookies = builder.cookies;
+        downloadFrom = builder.downloadFrom;
+        metadata = builder.metadata;
+        split = builder.split;
+        userPassword = builder.userPassword;
+        ownerPassword = builder.ownerPassword;
+        embeds = builder.embeds;
     }
 
 
@@ -48,7 +68,17 @@ public abstract class AbstractOptions {
         protected String extraHttpHeaders = null;
         protected String failOnConsoleExceptions = "false";
         protected String failOnHttpStatusCodes = "[499,599]";
+        protected String failOnResourceHttpStatusCodes = null;
+        protected String failOnResourceLoadingFailed = null;
         protected String skipNetworkIdleEvent = "false";
+        protected String generateDocumentOutline = null;
+        protected String cookies = null;
+        protected String downloadFrom = null;
+        protected String metadata = null;
+        protected String split = null;
+        protected String userPassword = null;
+        protected String ownerPassword = null;
+        protected List<File> embeds = null;
 
         /**
          * Sets the header file to be used by Chromium.
@@ -108,8 +138,8 @@ public abstract class AbstractOptions {
          * @param waitForExpression JavaScript's expression to wait before converting a document into PDF until it returns true.
          * @return The Builder instance for method chaining.
          */
-        public Builder<T> addWaitForExpression(JSObject waitForExpression) {
-            this.waitForExpression = waitForExpression.toString();
+        public Builder<T> addWaitForExpression(String waitForExpression) {
+            this.waitForExpression = waitForExpression;
             return this;
         }
 
@@ -164,6 +194,124 @@ public abstract class AbstractOptions {
          */
         public Builder<T> addSkipNetworkIdleEvent(boolean skipNetworkIdleEvent) {
             this.skipNetworkIdleEvent = String.valueOf(skipNetworkIdleEvent);
+            return this;
+        }
+
+        /**
+         * Sets the HTTP status codes to fail on for resources.
+         *
+         * @param failOnResourceHttpStatusCodes HTTP status codes to fail on for resources.
+         * @return The Builder instance for method chaining.
+         */
+        public Builder<T> addFailOnResourceHttpStatusCodes(ArrayList<Integer> failOnResourceHttpStatusCodes) {
+            StringBuilder sb = new StringBuilder("[");
+            for (int i = 0; i < failOnResourceHttpStatusCodes.size(); i++) {
+                sb.append(failOnResourceHttpStatusCodes.get(i));
+                if (i < failOnResourceHttpStatusCodes.size() - 1) {
+                    sb.append(",");
+                }
+            }
+            sb.append("]");
+            this.failOnResourceHttpStatusCodes = sb.toString();
+            return this;
+        }
+
+        /**
+         * Sets whether to fail on resource loading failed.
+         *
+         * @param failOnResourceLoadingFailed `true` to fail on resource loading failed, `false` otherwise.
+         * @return The Builder instance for method chaining.
+         */
+        public Builder<T> addFailOnResourceLoadingFailed(boolean failOnResourceLoadingFailed) {
+            this.failOnResourceLoadingFailed = String.valueOf(failOnResourceLoadingFailed);
+            return this;
+        }
+
+        /**
+         * Sets whether to generate document outline.
+         *
+         * @param generateDocumentOutline `true` to generate document outline, `false` otherwise.
+         * @return The Builder instance for method chaining.
+         */
+        public Builder<T> addGenerateDocumentOutline(boolean generateDocumentOutline) {
+            this.generateDocumentOutline = String.valueOf(generateDocumentOutline);
+            return this;
+        }
+
+        /**
+         * Sets the cookies to be used by Chromium.
+         *
+         * @param cookies List of cookies as JSON string.
+         * @return The Builder instance for method chaining.
+         */
+        public Builder<T> addCookies(String cookies) {
+            this.cookies = cookies;
+            return this;
+        }
+
+        /**
+         * Sets the download from configuration.
+         *
+         * @param downloadFrom Download from configuration as JSON string.
+         * @return The Builder instance for method chaining.
+         */
+        public Builder<T> addDownloadFrom(String downloadFrom) {
+            this.downloadFrom = downloadFrom;
+            return this;
+        }
+
+        /**
+         * Sets the metadata to be written.
+         *
+         * @param metadata Metadata as JSON string.
+         * @return The Builder instance for method chaining.
+         */
+        public Builder<T> addMetadata(String metadata) {
+            this.metadata = metadata;
+            return this;
+        }
+
+        /**
+         * Sets the split configuration.
+         *
+         * @param split Split configuration as JSON string.
+         * @return The Builder instance for method chaining.
+         */
+        public Builder<T> addSplit(String split) {
+            this.split = split;
+            return this;
+        }
+
+        /**
+         * Sets the user password for opening the resulting PDF(s).
+         *
+         * @param userPassword User password.
+         * @return The Builder instance for method chaining.
+         */
+        public Builder<T> addUserPassword(String userPassword) {
+            this.userPassword = userPassword;
+            return this;
+        }
+
+        /**
+         * Sets the owner password for full access on the resulting PDF(s).
+         *
+         * @param ownerPassword Owner password.
+         * @return The Builder instance for method chaining.
+         */
+        public Builder<T> addOwnerPassword(String ownerPassword) {
+            this.ownerPassword = ownerPassword;
+            return this;
+        }
+
+        /**
+         * Sets the files to embed in the generated PDF.
+         *
+         * @param embeds List of files to embed.
+         * @return The Builder instance for method chaining.
+         */
+        public Builder<T> addEmbeds(List<File> embeds) {
+            this.embeds = embeds;
             return this;
         }
 
---
+++ src/main/java/dev/inaka/common/Split.java
@@ -0,0 +1,61 @@
+package dev.inaka.common;
+
+/**
+ * Represents a split configuration for PDF operations.
+ */
+public class Split {
+    private final String mode; // 'pages' or 'intervals'
+    private final String span;
+    private final Boolean unify;
+    private final Boolean flatten;
+
+    private Split(Builder builder) {
+        this.mode = builder.mode;
+        this.span = builder.span;
+        this.unify = builder.unify;
+        this.flatten = builder.flatten;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public String getSpan() {
+        return span;
+    }
+
+    public Boolean getUnify() {
+        return unify;
+    }
+
+    public Boolean getFlatten() {
+        return flatten;
+    }
+
+    public static class Builder {
+        private String mode;
+        private String span;
+        private Boolean unify;
+        private Boolean flatten;
+
+        public Builder(String mode, String span) {
+            this.mode = mode;
+            this.span = span;
+        }
+
+        public Builder unify(Boolean unify) {
+            this.unify = unify;
+            return this;
+        }
+
+        public Builder flatten(Boolean flatten) {
+            this.flatten = flatten;
+            return this;
+        }
+
+        public Split build() {
+            return new Split(this);
+        }
+    }
+}
+
