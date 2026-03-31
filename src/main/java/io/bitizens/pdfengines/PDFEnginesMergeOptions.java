6e7cfda622ddec1206d891bffad90fbb34c120a1
Taha Cherfia <taha.cherfia@gmail.com>
2026-03-31 17:08:18 +0200

    feat(pdf-engines): add support for bookmarks and auto-index bookmarks

--- src/main/java/io/bitizens/pdfengines/PDFEnginesMergeOptions.java
+++ src/main/java/io/bitizens/pdfengines/PDFEnginesMergeOptions.java
@@ -1,5 +1,6 @@
 package io.bitizens.pdfengines;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
@@ -8,12 +9,16 @@ import org.json.JSONObject;
 public final class PDFEnginesMergeOptions extends PDFEnginesOptions {
     private final String metadata;
     private final String flatten;
+    private final String bookmarks;
+    private final String autoIndexBookmarks;
     private final String downloadFrom;
 
     private PDFEnginesMergeOptions(Builder builder) {
         super(builder);
         metadata = builder.metadata;
         flatten = builder.flatten;
+        bookmarks = builder.bookmarks;
+        autoIndexBookmarks = builder.autoIndexBookmarks;
         downloadFrom = builder.downloadFrom;
     }
 
@@ -23,6 +28,8 @@ public final class PDFEnginesMergeOptions extends PDFEnginesOptions {
     public static class Builder extends PDFEnginesOptions.Builder<PDFEnginesMergeOptions> {
         private String metadata = null;
         private String flatten = null;
+        private String bookmarks = null;
+        private String autoIndexBookmarks = "false";
         private String downloadFrom = null;
 
         /**
@@ -44,6 +51,39 @@ public final class PDFEnginesMergeOptions extends PDFEnginesOptions {
          */
         public Builder addFlatten(boolean flatten) {
             this.flatten = String.valueOf(flatten);
+            return this;
+        }
+
+        /**
+         * Sets custom bookmarks JSON for merge operation.
+         *
+         * @param bookmarks The bookmarks JSON.
+         * @return The Builder instance for method chaining.
+         */
+        public Builder addBookmarks(JSONArray bookmarks) {
+            this.bookmarks = String.valueOf(bookmarks);
+            return this;
+        }
+
+        /**
+         * Sets custom bookmarks JSON map for merge operation.
+         *
+         * @param bookmarks The bookmarks JSON map.
+         * @return The Builder instance for method chaining.
+         */
+        public Builder addBookmarks(JSONObject bookmarks) {
+            this.bookmarks = String.valueOf(bookmarks);
+            return this;
+        }
+
+        /**
+         * Sets whether existing bookmarks should be auto-indexed from input files.
+         *
+         * @param autoIndexBookmarks `true` to auto-index bookmarks, `false` otherwise.
+         * @return The Builder instance for method chaining.
+         */
+        public Builder addAutoIndexBookmarks(boolean autoIndexBookmarks) {
+            this.autoIndexBookmarks = String.valueOf(autoIndexBookmarks);
             return this;
         }
 
